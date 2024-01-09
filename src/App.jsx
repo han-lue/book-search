9af6e0b39f4b15ef57199bfd676c63c54a4bdfa9
username <denesakin@gmail.com>
2024-01-09 23:24:41 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className='bg-black h-screen w-screen text-white'>
+      test
+    </div>
+  )
+}
+
+export default App
